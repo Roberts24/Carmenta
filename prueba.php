150baf166efc412e3918f04a8a6c0225d2b1a532
jocampos14 <96193254+jocampos14@users.noreply.github.com>
2021-12-15 17:04:55 +0100

    Create prueba.php

---
+++ prueba.php
@@ -0,0 +1,16 @@
+<!doctype html>
+ <html lang="es">
+    <head>
+        <meta charset="utf-8">
+            <title>Título</title>
+    </head>
+    <body>
+      <!-- Arriba españita-->
+         <!-- muestra un número aleatorio de dos -->
+        <?php
+            $numero=mt_rand(0,1);
+            if ($numero==0) echo "<img src='Imagenes/img1.jpg'>";
+            else echo "<img src='Imagenes/img2.jpg'>";
+        ?>
+    </body>
+</html>
